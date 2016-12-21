339b0a4efd1d740a5d50e0ba2c252b64006307c6
gguo <gguo@movoto.com>
2016-12-21 17:07:00 +0800

    added dash quote

--- extension.js
+++ extension.js
@@ -6,17 +6,13 @@ var vscode = require('vscode');
 // your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "gg-bracket-selection" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     var disposable = vscode.commands.registerTextEditorCommand('extension.selectTextInBracket', function (textEditor, edit) {
         // The code you place here will be executed every time your command is executed
-        const leftBrakets = ['{', '[', '(', '<', "'", '"'];
-        const rightBrakets = ['}', ']', ')', '>', "'", '"'];
+        const leftBrakets = ['{', '[', '(', '<', "'", '"', '`'];
+        const rightBrakets = ['}', ']', ')', '>', "'", '"', '`'];
         var { document, selection } = textEditor;
         let posStack = [], startCharater = -1, endCharater = -1;
         var curPos = textEditor.selection.active;
@@ -30,6 +26,8 @@ function activate(context) {
                 leftBrakets[4] = 'LSQ';
             } else if(leftType === 5){
                 leftBrakets[5] = 'LQ';
+            } else if(leftType === 6){
+                leftBrakets[6] = 'DQ';
             }
 
             if (leftType >= 0) {
